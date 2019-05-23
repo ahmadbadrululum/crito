641b53769993b36ef8d2ed2e8c7ab771d2b0ac9a
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-05-23 11:10:10 +0700

    template login register

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -4,42 +4,7 @@
 
 <body class="layout-3">
     <div id="app">
-        <div class="navbar-bg" style="height:70px;"></div>
-        <nav class="navbar navbar-expand-lg main-navbar">
-            <a href="{{ url('/') }}" class="navbar-brand sidebar-gone-hide">Crito</a>
-
-            <ul class="navbar-nav navbar-right ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-                @if (Route::has('register'))
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-                @endif
-                @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-                @endguest
-            </ul>
-        </nav>
+    @include('auth.navbar')
         <main class="py-4">
             @yield('content')
         </main>
